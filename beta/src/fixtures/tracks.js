3e3c97b436a773371624966b1e41244147b21dd1
Augustin Godiscal <hello@auggod.tech>
2022-07-08 14:59:17 +0200

    Track id should be number 6

--- beta/src/fixtures/tracks.js
+++ beta/src/fixtures/tracks.js
@@ -8,17 +8,17 @@ module.exports = [
         title: 'Album title'
       }
     ],
-    url: 'https://stream.justifay.com/api/stream/5',
+    url: 'https://stream.justifay.com/api/stream/6',
     track: {
       album: 'Album title',
-      id: 5,
+      id: 6,
       creator_id: 1,
       title: 'Happy Song',
       duration: 315.48,
       year: 2018,
       artist: 'TazLazuli',
       status: 'Paid',
-      url: 'https://stream.justifay.com/api/stream/5',
+      url: 'https://stream.justifay.com/api/stream/6',
       cover: 'https://i1.sndcdn.com/artworks-000251447893-n8ad3s-t500x500.jpg',
       images: {
         small: {
